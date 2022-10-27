9807f1bf6f636394080aaec194ce9956a5484405
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2022-10-28 01:38:57 +0200

    Esercice 64. Competly done to CASA. Waiting for (ufficialy) correction

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+//import java.util.TreeMap;
 
 /**
  * 
@@ -28,7 +29,6 @@ import java.util.Set;
 public final class SocialNetworkUserImpl<U extends User> extends UserImpl implements SocialNetworkUser<U> {
 
     /*
-     *
      * [FIELDS]
      *
      * Define any necessary field
@@ -36,7 +36,7 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      * In order to save the people followed by a user organized in groups, adopt
      * a generic-type Map:  think of what type of keys and values would best suit the requirements
      */
-
+    private Map<String, Set<U>> follower;
     /*
      * [CONSTRUCTORS]
      *
@@ -62,12 +62,16 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      *            application
      */
     public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(null, null, null, 0);
+        super(name, surname, user, userAge);
+        this.follower = new HashMap<>();
     }
 
     /*
      * 2) Define a further constructor where the age defaults to -1
      */
+    public SocialNetworkUserImpl(final String name, final String surname, final String user) {
+        this(name, surname, user, -1);
+    }
 
     /*
      * [METHODS]
@@ -76,7 +80,12 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public boolean addFollowedUser(final String circle, final U user) {
-        return false;
+        Set<U> tmp =  follower.get(circle);
+        if (tmp == null) {
+            tmp = new HashSet<>();
+            follower.put(circle, tmp);
+        } 
+        return tmp.add(user);
     }
 
     /**
@@ -86,11 +95,24 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
+        final Collection<U> tmp =  follower.get(groupName);
+        if(tmp!=null){
+            return new ArrayList<>(tmp);
+        } else {
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public List<U> getFollowedUsers() {
-        return null;
+        List<U> tmp = new ArrayList<>();
+        if (!this.follower.isEmpty()) {
+            for (Set<U> circle : follower.values()) {
+                for(U user : circle){
+                    tmp.add(user);
+                }
+            }
+        } 
+        return tmp;
     }
 }
